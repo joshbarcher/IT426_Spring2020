7d5c562aef439ffc7bc1e97605368e2021d727c6
Josh Archer <jarcher@greenriver.edu>
2020-04-08 11:33:52 -0700

    Added a thymeleaf template and view variables.

--- messaging-app/src/main/java/edu/greenriver/it/messagingapp/controllers/HomeController.java
+++ messaging-app/src/main/java/edu/greenriver/it/messagingapp/controllers/HomeController.java
@@ -1,14 +1,26 @@
 package edu.greenriver.it.messagingapp.controllers;
 
+import edu.greenriver.it.messagingapp.repositories.IMessagesRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController
 {
-    @RequestMapping("/")
-    public String home()
+    private IMessagesRepository repo;
+
+    public HomeController(IMessagesRepository repo)
     {
+        this.repo = repo;
+    }
+
+    @RequestMapping("/")
+    public String home(Model model)
+    {
+        //save all messages from the repo to our view variable
+        model.addAttribute("messages", repo.findAll());
+
         //the job of this method is to return the name
         //of a template
         return "index";
